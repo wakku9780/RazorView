c58ca27c76fddbac64a60ad01896e571d5a1a81d
waqar <wakku9780@gmail.com>
2024-08-19 14:52:21 +0530

    Add project files.

---
+++ scratch/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+/*pp.MapGet("/", () => "Hello World!");*/
+
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
---
+++ scratch/controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scratch.controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About() {
+            return View();
+                }
+
+        public string  Details()
+        {
+            return "this is waqar";
+        }
+    }
+}
+
+
+
+
+
+
