16dd8401b38e5ce6164bb0b66599c5ab8889b016
waqar <wakku9780@gmail.com>
2024-08-20 15:28:21 +0530

    till temp data complete

--- scratch/controllers/HomeController.cs
+++ scratch/controllers/HomeController.cs
@@ -6,6 +6,38 @@ namespace scratch.controllers
     {
         public IActionResult Index()
         {
+
+
+            ViewBag.data5 = "tension";
+            ViewBag.data6 = new List<string>()
+            {
+                "cricket","hockey","football"
+            };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ViewData["data1"] = "program";
+            ViewData["data2"] = "waqar";
+
+            ViewData["data3"] = DateTime.Now.ToLongDateString();
+
+            string[] arr = { "waqar", "hashim", "mp" };
+            ViewData["data4"] = arr;
+
+              
+
             return View();
         }
 
@@ -16,6 +48,11 @@ namespace scratch.controllers
         public string  Details()
         {
             return "this is waqar";
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
         }
     }
 }
